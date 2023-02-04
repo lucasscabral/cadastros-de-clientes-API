d60f8b4ca20c327c8a52e7ca99f6d5a510be48c3
lucasscabral <lucas.9cabral@gmail.com>
2023-02-04 14:03:54 -0200

    Cria Projeto de Cadastro de client

---
+++ src/main/java/com/api/cadastra_cliente_api/CadastraClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.cadastra_cliente_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastraClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastraClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cadastra_cliente_api/CadastraClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cadastra_cliente_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastraClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
