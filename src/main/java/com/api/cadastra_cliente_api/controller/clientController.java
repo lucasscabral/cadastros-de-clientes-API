e884195aa3904ff057ac04a891e0332edcf2279e
lucasscabral <lucas.9cabral@gmail.com>
2023-02-05 02:34:41 -0200

    feat: Implementa rota de buscar cliente pelo CPF

--- src/main/java/com/api/cadastra_cliente_api/controller/clientController.java
+++ src/main/java/com/api/cadastra_cliente_api/controller/clientController.java
@@ -1,5 +1,7 @@
 package com.api.cadastra_cliente_api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,11 +35,23 @@ public class clientController {
         return ResponseEntity.ok(service.getAllClient(pageable));
     }
 
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Object> getClientByCpf(@PathVariable String cpf) {
+        List<client> clientCpf = service.getClientByCpf(cpf);
+
+        if (clientCpf.isEmpty()) {
+            return ResponseEntity.status(404).body("Usuário não encontrado com esse CPF");
+        } else {
+            return ResponseEntity.status(200).body(clientCpf);
+        }
+
+    }
+
     @PostMapping
     public ResponseEntity<String> createCustomer(@RequestBody @Valid clientDTO req) {
 
         String isValid = service.clientRegister(req);
-        if (isValid.equals("Já existe um usuário com esse cpf")) {
+        if (isValid != "ok") {
             return ResponseEntity.status(401).body(isValid);
         } else {
             return ResponseEntity.status(201).body(isValid);
