37a7a350fb1a2260183ba9f518c4a108c23007a0
lucasscabral <lucas.9cabral@gmail.com>
2023-02-07 12:28:20 -0200

    fix: Pega as mensagens da variável de ambiente do projeto e adiciona um Schema de descrição dos campos

--- src/main/java/com/api/cadastra_cliente_api/dto/clientDTO.java
+++ src/main/java/com/api/cadastra_cliente_api/dto/clientDTO.java
@@ -2,14 +2,15 @@ package com.api.cadastra_cliente_api.dto;
 
 import java.sql.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record clientDTO(
-        @Pattern(regexp = "[A-Za-z].* [A-Za-z].*", message = "Deve ter nome e sobrenome") @Size(min = 4, message = "O nome deve ter no minimo 4 caracteres") @NotBlank(message = "O Campo 'name' é obrigatório") String name,
-        @NotBlank(message = "O Campo 'cpf' é obrigatório") String cpf,
-        @Past(message = "Deve ser uma data válida e o formato da data deve ser no formato americano 'yyyy-mm-dd'") Date birthDate) {
+                @Pattern(regexp = "[A-Za-z].* [A-Za-z].*", message = "{firstName_lastName}") @Size(min = 4, message = "{minCharaterName}") @NotBlank(message = "{name.not.blank}") @Schema(description = "Esse é um campo do nome do Cliente", example = "Joãozinho da silva") String name,
+                @NotBlank(message = "{cpf.not.blank}") @Schema(description = "Esse é um campo do CPF do Cliente", example = "111.444.777-01") String cpf,
+                @Past(message = "{birthDate.format.invalid}") @Schema(description = "Esse é um campo da data de nascimento do Cliente", example = "2000-10-12") Date birthDate) {
 
 }
